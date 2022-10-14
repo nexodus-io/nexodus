d9333cadd7322532e41468d6e46da16b28fc7b1a
Brent Salisbury <bsalisbu@redhat.com>
2022-10-14 02:05:31 -0400

    Debugging IT flake

--- tests/e2e-scripts/init-containers.sh
+++ tests/e2e-scripts/init-containers.sh
@@ -202,6 +202,12 @@ setup_custom_zone_connectivity() {
     sudo docker exec node1 killall aircrew
     sudo docker exec node2 killall aircrew
 
+    # STDOUT the run scripts for debugging
+    echo "=== Displaying aircrew-run-node1.sh ==="
+    cat aircrew-run-node1.sh
+    echo "=== Displaying aircrew-run-node2.sh ==="
+    cat aircrew-run-node2.sh
+
     sudo docker cp ./aircrew-run-node1.sh node1:/bin/aircrew-run-node1.sh
     sudo docker cp ./aircrew-run-node2.sh node2:/bin/aircrew-run-node2.sh
     sudo docker cp ./node1-private.key node1:/etc/wireguard/private.key
@@ -269,6 +275,12 @@ setup_custom_second_zone_connectivity() {
     sudo docker exec node1 killall aircrew
     sudo docker exec node2 killall aircrew
 
+    # STDOUT the run scripts for debugging
+    echo "=== Displaying aircrew-run-node1.sh ==="
+    cat aircrew-run-node1.sh
+    echo "=== Displaying aircrew-run-node2.sh ==="
+    cat aircrew-run-node2.sh
+
     sudo docker cp ./aircrew-run-node1.sh node1:/bin/aircrew-run-node1.sh
     sudo docker cp ./aircrew-run-node2.sh node2:/bin/aircrew-run-node2.sh
     sudo docker cp ./node1-private.key node1:/etc/wireguard/private.key
@@ -315,6 +327,9 @@ setup_child_prefix_connectivity() {
     local node2_pvtkey=WBydF4bEIs/uSR06hrsGa4vhgNxgR6rmR68CyOHMK18=
     local node2_ip=$(sudo docker inspect --format "{{ .NetworkSettings.IPAddress }}" node2)
 
+    # Delete the ipam storage in the case the run has re-run since we dont overwrite existing child-prefix
+    rm -rf prefix-test.json
+
     # Create the new zone with a CGNAT range
     curl -L -X POST 'http://localhost:8080/zone' \
     -H 'Content-Type: application/json' \
@@ -358,6 +373,14 @@ EOF
     --zone=${zone}
 EOF
 
+    # STDOUT the run scripts for debugging
+    echo "=== Displaying aircrew-run-node1.sh ==="
+    cat aircrew-run-node1.sh
+    echo "=== Displaying aircrew-run-node2.sh ==="
+    cat aircrew-run-node2.sh
+    echo "=== Displaying ipam persistent storage ==="
+
+    # Copy files to the containers
     sudo docker cp ./aircrew-run-node1.sh node1:/bin/aircrew-run-node1.sh
     sudo docker cp ./aircrew-run-node2.sh node2:/bin/aircrew-run-node2.sh
     sudo docker cp ./node1-private.key node1:/etc/wireguard/private.key
