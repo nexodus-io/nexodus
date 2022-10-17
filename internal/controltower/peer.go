37cb04ec5acb18dbb248e87743ec72a10fdf6d5d
Dave Tucker <dave@dtucker.co.uk>
2022-10-17 20:07:44 +0100

    Add PubKeys REST API and UI (#57)
    Signed-off-by: Dave Tucker <dave@dtucker.co.uk>

--- internal/controltower/peer.go
+++ internal/controltower/peer.go
@@ -1,8 +1,6 @@
 package controltower
 
 import (
-	"fmt"
-
 	"github.com/google/uuid"
 )
 
@@ -27,49 +25,4 @@ func NewPeer(publicKey, endpointIP, allowedIPs, zone, nodeAddress, childPrefix s
 		NodeAddress: nodeAddress,
 		ChildPrefix: childPrefix,
 	}
-}
-
-type PeerMap struct {
-	cache   map[uuid.UUID]*Peer
-	pubKeys map[string]map[uuid.UUID]struct{}
-}
-
-func NewPeerMap() *PeerMap {
-	return &PeerMap{
-		cache:   make(map[uuid.UUID]*Peer),
-		pubKeys: make(map[string]map[uuid.UUID]struct{}),
-	}
-}
-
-func (m *PeerMap) Insert(p Peer) {
-	m.cache[p.ID] = &p
-	if _, ok := m.pubKeys[p.PublicKey]; !ok {
-		m.pubKeys[p.PublicKey] = make(map[uuid.UUID]struct{})
-	}
-	m.pubKeys[p.PublicKey][p.ID] = struct{}{}
-}
-
-func (m *PeerMap) List() []*Peer {
-	res := make([]*Peer, 0)
-	for _, v := range m.cache {
-		res = append(res, v)
-	}
-	return res
-}
-
-func (m *PeerMap) ListByPubKey(key string) []*Peer {
-	res := make([]*Peer, 0)
-	if v, ok := m.pubKeys[key]; ok {
-		for pid := range v {
-			res = append(res, m.cache[pid])
-		}
-	}
-	return res
-}
-
-func (m *PeerMap) Get(id uuid.UUID) (*Peer, error) {
-	if peer, ok := m.cache[id]; ok {
-		return peer, nil
-	}
-	return nil, fmt.Errorf("peer not found")
 }
--- ui/src/peers.js
+++ ui/src/peers.js
@@ -1,10 +1,14 @@
-import { Datagrid, List, Show, SimpleShowLayout, TextField } from 'react-admin';
+import { Datagrid, List, ReferenceField, Show, SimpleShowLayout, TextField, ReferenceInput } from 'react-admin';
+
+const peerFilters =  [
+    <ReferenceInput source="public-key" label="Public Key" reference="pubkeys" />,,
+]
 
 export const PeerList = () => (
-    <List>
+    <List filters={peerFilters}>
         <Datagrid rowClick="show" bulkActionButtons={false} >
             <TextField label="ID" source="id" />
-            <TextField label="Public Key" source="public-key" />
+            <ReferenceField label="Public Key" source="public-key" reference='pubkeys' />
             <TextField label="Endpoint IP" source="endpoint-ip" />
             <TextField label="Allowed IPs" source="allowed-ips" />
             <TextField label="Zone" source="zone" />
@@ -18,7 +22,7 @@ export const PeerShow= () => (
     <Show>
         <SimpleShowLayout>
             <TextField label="ID" source="id" />
-            <TextField label="Public Key" source="public-key" />
+            <ReferenceField label="Public Key" source="public-key" reference='pubkeys' />
             <TextField label="Endpoint IP" source="endpoint-ip" />
             <TextField label="Allowed IPs" source="allowed-ips" />
             <TextField label="Zone" source="zone" />
