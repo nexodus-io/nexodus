c9846a830d5cb2b7f3e60fa9bbfc96eee6b63599
Dave Tucker <dave@dtucker.co.uk>
2022-10-20 10:55:33 +0000

    keycloak: Add api-clients client for tokens
    Signed-off-by: Dave Tucker <dave@dtucker.co.uk>

---
+++ hack/keycloak-grant.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+HOST="localhost:8888"
+REALM="controltower"
+USERNAME="$1"
+PASSWORD="$2"
+CLIENTID='api-clients'
+CLIENTSECRET='cvXhCRXI2Vld244jjDcnABCMrTEq2rwE'
+
+curl -s -X POST \
+    http://$HOST/realms/$REALM/protocol/openid-connect/token \
+    -H 'Content-Type: application/x-www-form-urlencoded' \
+    -d "username=$USERNAME" \
+    -d "password=$PASSWORD" \
+    -d "grant_type=password" \
+    -d "client_id=$CLIENTID" \
+    -d "client_secret=$CLIENTSECRET" | jq -r ".access_token"
