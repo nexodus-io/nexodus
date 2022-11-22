4c86d4783f40d48f20f4ff5716e5f6d17276a970
Russell Bryant <rbryant@redhat.com>
2022-11-22 12:17:48 -0500

    ui: Drop ID from peer list
    Drop the final UUID shown on any of the resource listing pages. I also
    reordered the columns to show the Device and Zone first, as those are
    the key items that define a Peer.
    
    Signed-off-by: Russell Bryant <rbryant@redhat.com>

--- ui/src/pages/Peers.tsx
+++ ui/src/pages/Peers.tsx
@@ -7,11 +7,10 @@ const peerFilters =  [
 export const PeerList = () => (
     <List filters={peerFilters}>
         <Datagrid rowClick="show" bulkActionButtons={false} >
-            <TextField label="ID" source="id" />
             <ReferenceField label="Device" source="device_id" reference='devices' link="show"/>
+            <ReferenceField label="Zone" source="zone_id" reference='zones' link="show" />
             <TextField label="Endpoint IP" source="endpoint_ip" />
             <TextField label="Allowed IPs" source="allowed_ips" />
-            <ReferenceField label="Zone" source="zone_id" reference='zones' link="show" />
             <TextField label="Node Address" source="node_address" />
             <TextField label="STUN Address" source="reflexive_ip4" />
             <TextField label="Child Prefix" source="child_prefix" />
@@ -24,9 +23,9 @@ export const PeerShow= () => (
         <SimpleShowLayout>
             <TextField label="ID" source="id" />
             <ReferenceField label="Device" source="device_id" reference='devices' link="show" />
+            <ReferenceField label="Zone" source="zone_id" reference='zones' link="show" />
             <TextField label="Endpoint IP" source="endpoint_ip" />
             <TextField label="Allowed IPs" source="allowed_ips" />
-            <ReferenceField label="Zone" source="zone_id" reference='zones' link="show" />
             <TextField label="Node Address" source="node_address" />
             <TextField label="STUN Address" source="reflexive_ip4" />
             <TextField label="Child Prefix" source="child_prefix" />
