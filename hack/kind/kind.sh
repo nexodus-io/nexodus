7dd83696190dd0271e1721e9605f331486987db4
Anil Vishnoi <avishnoi@redhat.com>
2022-12-23 09:56:51 -0800

    Add check for dependencies to ensure required dependencies are installed. (#275)
    * Kubectl is dependency. Add check to ensure its installed
    
    Signed-off-by: Anil Kumar Vishnoi <avishnoi@redhat.com>
    
    * Add explicit check for Kind
    
    Signed-off-by: Anil Kumar Vishnoi <avishnoi@redhat.com>
    
    Signed-off-by: Anil Kumar Vishnoi <avishnoi@redhat.com>

--- hack/kind/kind.sh
+++ hack/kind/kind.sh
@@ -1,18 +1,53 @@
 #!/bin/sh
 
-set -e
+set -ex
+info_message() {
+    if [ -z "${1}" ]; then
+        echo "info_message() requires a message"
+        exit 1
+    fi
+    echo -e "[\033[0;34m ACTION \033[0m] ${1}"
+}
+
+pass_message() {
+    if [ -z "${1}" ]; then
+        echo "pass_message() requires a message"
+        exit 1
+    fi
+    echo -e "[\033[0;32m PASSED \033[0m] ${1}"
+}
+
+error_message() {
+    if [ -z "${1}" ]; then
+        echo "error_message() requires a message"
+        exit 1
+    fi
+    if [ -n "$1" ]; then
+        echo -e "[\033[0;31m FAILED \033[0m] ] ${1}"
+    fi
+}
 
 up() {
+    if ! [ -x "$(command -v kind)" ]; then
+        error_message "Kind is required. Please install Kind"
+        exit 1
+    fi
+
+    if ! [ -x "$(command -v kubectl)" ]; then
+        error_message "kubectl is required. Please install kubectl"
+        exit 1
+    fi
+
     kind create cluster --config ./deploy/kind.yaml
     kubectl cluster-info --context kind-apex-dev
 
     trap "kubectl get pods -A" EXIT
 
-    echo "Deploying Ingress Controller"
+    info_message "Deploying Ingress Controller"
     kubectl apply -f ./deploy/kind-ingress.yaml
     kubectl rollout status deployment ingress-nginx-controller -n ingress-nginx --timeout=5m
 
-    echo "Adding Rewrite to CoreDNS"
+    info_message "Adding Rewrite to CoreDNS"
     kubectl get -n kube-system cm/coredns -o yaml > coredns.yaml
     sed -i '22i \
             rewrite name auth.apex.local dex.apex.svc.cluster.local' coredns.yaml
@@ -21,26 +56,26 @@ up() {
     kubectl rollout restart -n kube-system deployment/coredns
     kubectl rollout status -n kube-system deployment coredns --timeout=5m
 
-    echo "Installing Cert Manager"
+    info_message "Installing Cert Manager"
     kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.10.1/cert-manager.yaml
     kubectl rollout status -n cert-manager deploy/cert-manager --timeout=5m
     kubectl rollout status -n cert-manager deploy/cert-manager-webhook --timeout=5m
     kubectl wait --for=condition=Ready pods --all -n cert-manager --timeout=5m
 
-    echo "Installing Postgres Operator"
+    info_message "Installing Postgres Operator"
     kubectl apply -k https://github.com/CrunchyData/postgres-operator-examples/kustomize/install/namespace
     kubectl apply --server-side -k https://github.com/CrunchyData/postgres-operator-examples/kustomize/install/default
     kubectl wait --for=condition=Ready pods --all -n postgres-operator --timeout=5m
     
-    echo "Loading Images To KIND"
+    info_message "Loading Images To KIND"
     kind load --name apex-dev docker-image quay.io/apex/apiserver:latest
     kind load --name apex-dev docker-image quay.io/apex/frontend:latest
 
-    echo "Deploying Apex"
+    info_message "Deploying Apex"
     kubectl create namespace apex
     kubectl apply -k ./deploy/apex/overlays/dev
 
-    echo "Waiting for Apex Pod Readiness"
+    info_message "Waiting for Apex Pod Readiness"
     kubectl wait --for=condition=Ready pods --all -n apex -l app.kubernetes.io/part-of=apex --timeout=15m
 }
 
@@ -65,7 +100,7 @@ case $1 in
         cacert
         ;;
     *)
-        echo "command required. up, down, or cacert"
+        error_message "command required. up, down, or cacert"
         exit 1
         ;;
 esac
