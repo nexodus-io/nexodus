699744e5dcc3da656660d1a223d3783dc54ca2c0
Russell Bryant <russell.bryant@gmail.com>
2023-03-11 21:39:45 -0500

    util: Add CheckPeriodically()
    Add a new utility function, CheckPeriodically(). Adjust
    RunPeriodically() to be a simpler wrapper around the new function.
    
    CheckPeriodically() can be used to periodically check for a condition to
    become true. It can also return an error.
    
    Signed-off-by: Russell Bryant <russell.bryant@gmail.com>

--- internal/util/run_periodically.go
+++ internal/util/run_periodically.go
@@ -6,14 +6,27 @@ import (
 )
 
 func RunPeriodically(ctx context.Context, duration time.Duration, fn func()) {
+	_, _ = CheckPeriodically(ctx, duration, func() (bool, error) {
+		fn()
+		return false, nil
+	})
+}
+
+// CheckPeriodically checks a function (fn) at a specified interval (duration).
+// It will return when one of these conditions occurs: fn returns true, fn returns
+// an error, the duration is met, or the context is Done().
+func CheckPeriodically(ctx context.Context, duration time.Duration, fn func() (bool, error)) (bool, error) {
 	ticker := time.NewTicker(duration)
 	defer ticker.Stop()
 	for {
 		select {
 		case <-ctx.Done():
-			return
+			return false, nil
 		case <-ticker.C:
-			fn()
+			cond, err := fn()
+			if cond || err != nil {
+				return cond, err
+			}
 		}
 	}
 }
