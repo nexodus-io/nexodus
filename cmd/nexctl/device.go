070128653b4bd1d5cf14bc2d64b706aa2ef68bd6
Brent Salisbury <bsalisbu@redhat.com>
2023-03-27 01:52:22 -0400

    Adds v6 support to nexctl
    Signed-off-by: Brent Salisbury <bsalisbu@redhat.com>

--- cmd/nexctl/device.go
+++ cmd/nexctl/device.go
@@ -15,12 +15,12 @@ func listOrgDevices(c *client.Client, organizationID uuid.UUID, encodeOut string
 	}
 	if encodeOut == encodeColumn || encodeOut == encodeNoHeader {
 		w := newTabWriter()
-		fs := "%s\t%s\t%s\t%s\t%s\t%s\t%s\n"
+		fs := "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n"
 		if encodeOut != encodeNoHeader {
-			fmt.Fprintf(w, fs, "DEVICE ID", "HOSTNAME", "NODE ADDRESS", "ENDPOINT IP", "PUBLIC KEY", "ORGANIZATION ID", "RELAY")
+			fmt.Fprintf(w, fs, "DEVICE ID", "HOSTNAME", "NODE ADDRESS IPV4", "NODE ADDRESS IPV6", "ENDPOINT IP", "PUBLIC KEY", "ORGANIZATION ID", "RELAY")
 		}
 		for _, dev := range devices {
-			fmt.Fprintf(w, fs, dev.ID, dev.Hostname, dev.TunnelIP, dev.LocalIP, dev.PublicKey, dev.OrganizationID, fmt.Sprintf("%t", dev.Relay))
+			fmt.Fprintf(w, fs, dev.ID, dev.Hostname, dev.TunnelIP, dev.TunnelIpV6, dev.LocalIP, dev.PublicKey, dev.OrganizationID, fmt.Sprintf("%t", dev.Relay))
 		}
 		w.Flush()
 
@@ -42,16 +42,18 @@ func listAllDevices(c *client.Client, encodeOut string) error {
 	}
 	if encodeOut == encodeColumn || encodeOut == encodeNoHeader {
 		w := newTabWriter()
-		fs := "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n"
+		fs := "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n"
 		if encodeOut != encodeNoHeader {
-			fmt.Fprintf(w, fs, "DEVICE ID", "HOSTNAME", "NODE ADDRESS", "ENDPOINT IP", "PUBLIC KEY", "ORGANIZATION ID",
-				"LOCAL IP", "ALLOWED IPS", "TUNNEL IP", "CHILD PREFIX", "ORGANIZATION PREFIX", "REFLEXIVE IPv4",
-				"ENDPOINT LOCAL IPv4", "RELAY")
+			fmt.Fprintf(w, fs, "DEVICE ID", "HOSTNAME", "NODE ADDRESS",
+				"ENDPOINT IP", "PUBLIC KEY", "ORGANIZATION ID",
+				"LOCAL IP", "ALLOWED IPS", "TUNNEL IPV4", "TUNNEL IPV6",
+				"CHILD PREFIX", "ORG PREFIX IPV4", "ORG PREFIX IPV6",
+				"REFLEXIVE IPv4", "ENDPOINT LOCAL IPv4", "RELAY")
 		}
 		for _, dev := range devices {
 			fmt.Fprintf(w, fs, dev.ID, dev.Hostname, dev.TunnelIP, dev.LocalIP, dev.PublicKey, dev.OrganizationID,
-				dev.LocalIP, dev.AllowedIPs, dev.TunnelIP, dev.ChildPrefix, dev.OrganizationPrefix, dev.ReflexiveIPv4,
-				dev.EndpointLocalAddressIPv4, fmt.Sprintf("%t", dev.Relay))
+				dev.LocalIP, dev.AllowedIPs, dev.TunnelIP, dev.TunnelIpV6, dev.ChildPrefix, dev.OrganizationPrefix,
+				dev.OrganizationPrefixV6, dev.ReflexiveIPv4, dev.EndpointLocalAddressIPv4, fmt.Sprintf("%t", dev.Relay))
 		}
 		w.Flush()
 
