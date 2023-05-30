cd318e44b211f99de61d98f74f2ae392faea4096
Russell Bryant <rbryant@redhat.com>
2023-05-30 14:42:36 -0400

    docs: Add nexctl usage docs
    Update the `nexctl` docs with usage information based on the help
    text. This doc can be automatically updated using the
    `contrib/nexctl-docs.sh` script.
    
    There is plenty of cleanup or additions that could be made. If the
    help text is improved or expanded, that will make its way to the doc
    site as well by running this script.
    
    Closes #1049
    
    Signed-off-by: Russell Bryant <rbryant@redhat.com>

---
+++ contrib/nexctl-docs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# remove all output after the line that says "## Usage"
+cat docs/user-guide/nexctl.md | sed -n '/## Usage/q;p' > docs/user-guide/nexctl.md.tmp
+
+printf "### Usage\n\n" >> docs/user-guide/nexctl.md.tmp
+
+# generate the usage output
+make dist/nexctl
+echo '```text' >> docs/user-guide/nexctl.md.tmp
+dist/nexctl -h | docker run -i --rm --name txt2man quay.io/nexodus/mock:latest txt2man -t nexctl | man -l - | cat >> docs/user-guide/nexctl.md.tmp
+echo '```' >> docs/user-guide/nexctl.md.tmp
+
+for subcmd in device invitation nexd organization user ; do
+    printf "\n#### nexctl $subcmd\n\n" >> docs/user-guide/nexctl.md.tmp
+    echo '```text' >> docs/user-guide/nexctl.md.tmp
+    dist/nexctl ${subcmd} -h | docker run -i --rm --name txt2man quay.io/nexodus/mock:latest txt2man -t nexctl-${subcmd} | man -l - | cat >> docs/user-guide/nexctl.md.tmp
+    echo '```' >> docs/user-guide/nexctl.md.tmp
+done
+
+mv docs/user-guide/nexctl.md.tmp docs/user-guide/nexctl.md
