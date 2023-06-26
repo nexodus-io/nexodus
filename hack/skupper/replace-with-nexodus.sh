90ee34e4392e0c194fd4d9aae82f57d7107a3d0e
Hiram Chirino <hiram@hiramchirino.com>
2023-06-26 12:58:47 -0400

    Apply the skupper-router labels to the nexodus router so that it’s targeted by services created by the skupper service controller.
    Also improved the PoC scripts so that your can fully replace the skupper router
    plane with the nexodus router.
    
    Signed-off-by: Hiram Chirino <hiram@hiramchirino.com>

---
+++ hack/skupper/replace-with-nexodus.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+kubectl config set-context --current --namespace west
+make deploy-nexd-router
+kubectl scale deployment skupper-router --replicas 0
+kubectl rollout status deployment skupper-router --timeout=1m
+
+kubectl config set-context --current --namespace east
+make deploy-nexd-router
+kubectl scale deployment skupper-router --replicas 0
+kubectl rollout status deployment skupper-router --timeout=1m
+
+kubectl port-forward $(kubectl get pods -l app=frontend -o name) 8080 || true&
+sleep 2
+open http://localhost:8080
