5a3cdd5f50278fdd68181f15286591b49579f20e
Russell Bryant <russell.bryant@gmail.com>
2023-08-03 11:59:31 -0400

    ui: switch to vite-plugin-favicons-inject
    The dependencies of vite-plugin-favicon have blocked resolution of
    multiple ui CVEs. This alternative does the same thing, but works with
    newer dependencies.
    
    This should resolve:
    
    - https://github.com/nexodus-io/nexodus/security/dependabot/15
    - https://github.com/nexodus-io/nexodus/security/dependabot/37
    - https://github.com/nexodus-io/nexodus/security/dependabot/47
    
    Signed-off-by: Russell Bryant <russell.bryant@gmail.com>

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
-import viteFaviconsPlugin from "vite-plugin-favicon";
+import vitePluginFaviconsInject from "vite-plugin-favicons-inject";
 import { Logout } from "react-admin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), viteFaviconsPlugin("./src/logo.png")],
+  plugins: [react(), viteTsconfigPaths(), vitePluginFaviconsInject("./src/logo.png")],
   build: {
     outDir: "build",
   },
