2bc78e16a4683a83e896358a0bcb7147fbedf3dc
Anil Kumar Vishnoi <avishnoi@redhat.com>
2023-08-25 15:25:31 -0400

    Support scripts for scale testing
    * Moved scale related scripts to scale directory
    * Add script to check connectivity within each org
    * Add script to scrap error from each container running nexd
    
    Signed-off-by: Anil Kumar Vishnoi <avishnoi@redhat.com>

---
+++ hack/e2e-scripts/scale/check-connectivity.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Script picks the first nexd container in each org/user and
+# check the peer connectivity using nexctl nexd peers ping command
+# and also dumps the wireguard wg dump with all the tx/rx counters.
+# Script is helpful during scale testing where multiple containers
+# are spawned across multiple organizations, and it's bit
+# time consuming to check if the peers are connected in the
+# existing orgs as we spwan new orgs.
+
+# Array to store encountered label values
+encountered_labels=()
+
+# Get a list of all container IDs, including stopped ones
+container_ids=$(docker ps -q)
+
+# Flag to print wg dump for tx/rx number
+print_wg=false
+
+print_usage() {
+    echo "Usage: $0 [-d]"
+    echo "  -d    Print wg dump information from the container"
+    exit 1
+}
+
+# Parse command line arguments
+while getopts "d" opt; do
+    case $opt in
+        d) print_wg=true ;;
+        \?) echo "Invalid option: -$OPTARG" >&2
+            print_usage ;;
+    esac
+done
+
+total_nexd=0
+total_orgs=0
+
+# Loop through each container
+for container_id in $container_ids; do
+    total_nexd=$((total_nexd + 1 ))
+
+    # Get the value of the "user" label for this container
+    label_value=$(docker inspect --format '{{ index .Config.Labels "user" }}' $container_id)
+
+    # If the label exists and the value is not in the encountered_labels array
+    if [ ! -z "$label_value" ] && [[ ! " ${encountered_labels[@]} " =~ " $label_value " ]]; then
+	total_orgs=$((total_orgs + 1))
+        # Add the label value to the encountered_labels array
+        encountered_labels+=("$label_value")
+
+	peers=$(docker  exec -it $container_id wg show wg0 dump |grep -v "off"| wc -l)
+	unreachable=$(docker exec $container_id nexctl nexd peers ping| grep "Unreachable"| wc -l)
+        # Print the container ID
+	if [ $unreachable -gt 0 ]; then
+		echo -e "container-d: $container_id    user:$label_value    peers:$peers    \e[91munreachable:$unreachable\e[0m"
+	else
+		echo -e "container-id: $container_id    user:$label_value    peers:$peers    \e[32munreachable:$unreachable\e[0m"
+	fi
+
+	if [ "$print_wg" = true ]; then
+		docker exec -it $container_id wg show wg0 dump
+	fi
+    fi
+done
+
+echo -e "\e[92m total-orgs: $total_orgs       total-nexd:$total_nexd\e[0m"
---
+++ hack/e2e-scripts/scale/check-errors.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Quick and dirty way to scrape the error messages from the nexd logs
+# from container.
+# As we spawn more containers for scale, it helps to keep checking the
+# errors in the existing running nexd in case they are getting impacted
+# by the scale and throwing errors.
+
+# Flag to print error logs from nexd logs
+print_log=false
+
+print_usage() {
+    echo "Usage: $0 [-l]"
+    echo "  -l    Print error log from nexd running in the container"
+    exit 1
+}
+
+# Parse command line arguments
+while getopts "l" opt; do
+    case $opt in
+        l) print_log=true ;;
+        \?) echo "Invalid option: -$OPTARG" >&2
+            print_usage ;;
+    esac
+done
+
+# Get a list of running container IDs
+container_ids=$(docker ps -q)
+
+echo -e "CONTAINER ID\tUSER\t\t\t\t\tERROR COUNT\tJSON\tEOF\tSTUN\tTIMEOUT\t400\t401\t429\t500\t501\t503\tNEW ERROR"
+# Loop through each container and execute the command
+for container_id in $container_ids; do
+    user_id=$(docker inspect --format '{{ index .Config.Labels "user" }}' $container_id)
+    logs=$(docker exec "$container_id" sh -c "cat nexodus-logs.txt | grep error" 2>&1)
+    error_400=$(echo "$logs" | grep -c "error: 400")
+    error_401=$(echo "$logs" | grep -c "error: 401")
+    error_429=$(echo "$logs" | grep -c "error: 429")
+    error_500=$(echo "$logs" | grep -c "error: 500")
+    error_501=$(echo "$logs" | grep -c "error: 501")
+    error_503=$(echo "$logs" | grep -c "error: 503")
+    error_json=$(echo "$logs" | grep -c "error: json")
+    error_eof=$(echo "$logs" | grep -c "EOF")
+    error_stun=$(echo "$logs" | grep -c "STUN")
+    error_timeout=$(echo "$logs" | grep -c "timeout")
+    known_error=$((error_400 + error_401 + error_429 + error_500 + error_501 + error_503 + error_json + error_eof + error_stun + error_timeout))
+    error_count=$(echo "$logs" | wc -l)
+
+    if [ "$error_count" -gt 0 ]; then
+	    if [ "$error_count" -eq "$known_error" ]; then
+		    echo -e "$container_id\t$user_id\t\t$error_count\t $error_json\t $error_eof\t $error_stun\t $error_timeout\t $error_400\t $error_401\t $error_429\t $error_500\t $error_501\t $error_503\t \e[32mNo\e[0m"
+	    else
+		echo -e "$container_id\t$user_id\t\t$error_count\t $error_json\t $error_eof\t $error_stun\t $error_timeout\t $error_400\t $error_401\t $error_429\t $error_500\t $error_501\t $error_503\t \e[91mYes\e[0m"
+	    fi
+
+	    if [ "$print_log" = true ]; then
+	    	echo "$logs"
+	    fi
+    fi
+done
