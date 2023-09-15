31e332930cc34b92fc1fa821529835051e54d832
Hiram Chirino <hiram@hiramchirino.com>
2023-09-15 10:40:58 -0400

    Move the redis fetch manager tests to the integration suite.
    Signed-off-by: Hiram Chirino <hiram@hiramchirino.com>

--- integration-tests/redis_fetch_manager_test.go
+++ integration-tests/redis_fetch_manager_test.go
@@ -1,29 +1,33 @@
-package redisfm
+//go:build integration
+
+package integration_tests
 
 import (
 	"context"
+	"github.com/nexodus-io/nexodus/internal/handlers/fetchmgr/redisfm"
 	"github.com/nexodus-io/nexodus/internal/handlers/fetchmgr/tests"
 	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 	"io"
+	"os"
 	"os/exec"
 	"testing"
 	"time"
 )
 
 func TestFetchManager(t *testing.T) {
+
 	t.Parallel()
-	require := require.New(t)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	cmd := exec.CommandContext(ctx, "kubectl", "-n", "nexodus", "port-forward", "redis-0", "6379:6379")
 	cmd.Stdout = io.Discard
-	cmd.Stderr = io.Discard
+	cmd.Stderr = os.Stderr
 	err := cmd.Start()
-	require.NoError(err)
+	require.NoError(t, err)
 
 	client := redis.NewClient(&redis.Options{
 		Addr: "127.0.0.1:6379",
@@ -33,5 +37,5 @@ func TestFetchManager(t *testing.T) {
 	_, _ = client.Del(context.Background(), "key1").Result()
 
 	logger, _ := zap.NewProductionConfig().Build()
-	tests.TestFetchManagerReducesDBFetchesAtTheTail(t, New(client, 1000*time.Second, logger, ""))
+	tests.TestFetchManagerReducesDBFetchesAtTheTail(t, redisfm.New(client, 1000*time.Second, logger, ""))
 }
