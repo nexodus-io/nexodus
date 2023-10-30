16e44ce18076844ab6b983f0f806603e47872a95
Brent Salisbury <bsalisbu@redhat.com>
2023-10-29 22:39:15 -0400

    Adjust nexctl to display the local node if it is an exit node
    - Currently, if the local node is an exit node it is omitted from
      the nexctl output of `nexctl nexd exit-node list`
    
    Signed-off-by: Brent Salisbury <bsalisbu@redhat.com>

--- internal/nexodus/ctlexit.go
+++ internal/nexodus/ctlexit.go
@@ -31,13 +31,42 @@ func (ac *NexdCtl) DisableExitNodeClient(_ string, result *string) error {
 	return nil
 }
 
+// ListExitNodes lists all exit node origins
 func (ac *NexdCtl) ListExitNodes(_ string, result *string) error {
-	exitNodeOrigins, err := json.Marshal(ac.nx.exitNode.exitNodeOrigins)
-	if err != nil {
-		return fmt.Errorf("error marshalling disable exit node results %w", err)
+	var allExitNodeOrigins []wgPeerConfig
+	isExitNode := false
+
+	// Check if the local node is an exit node
+	for _, prefix := range ac.nx.childPrefix {
+		if prefix == "0.0.0.0/0" {
+			isExitNode = true
+			break
+		}
 	}
 
-	*result = string(exitNodeOrigins)
+	// If the local node is an exit node, create a copy and append the new instance
+	if isExitNode {
+		// Make a copy of exitNodeOrigins to a new slice
+		allExitNodeOrigins = make([]wgPeerConfig, len(ac.nx.exitNode.exitNodeOrigins))
+		copy(allExitNodeOrigins, ac.nx.exitNode.exitNodeOrigins)
+
+		// Create a new instance of wgPeerConfig
+		newPeerConfig := wgPeerConfig{
+			PublicKey: ac.nx.wireguardPubKey,
+			Endpoint:  ac.nx.nodeReflexiveAddressIPv4.String(),
+		}
+		// Append the local node if it is an exit node
+		allExitNodeOrigins = append(allExitNodeOrigins, newPeerConfig)
+	} else {
+		allExitNodeOrigins = ac.nx.exitNode.exitNodeOrigins
+	}
+
+	exitNodeOriginsJSON, err := json.Marshal(allExitNodeOrigins)
+	if err != nil {
+		return fmt.Errorf("error marshalling exit node list results: %w", err)
+	}
+
+	*result = string(exitNodeOriginsJSON)
 
 	return nil
 }
