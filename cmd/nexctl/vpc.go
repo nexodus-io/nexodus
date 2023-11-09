ce7c4fb6dba78ef50cae1882d79554330109a584
Russell Bryant <rbryant@redhat.com>
2023-11-08 19:49:51 -0500

    nexctl: remove sec group update for vpc
    Signed-off-by: Russell Bryant <rbryant@redhat.com>

--- cmd/nexctl/vpc.go
+++ cmd/nexctl/vpc.go
@@ -43,19 +43,14 @@ func createVpcCommand() *cli.Command {
 						Name:     "ipv6-cidr",
 						Required: false,
 					},
-					&cli.StringFlag{
-						Name:     "security-group-id",
-						Required: false,
-					},
 				},
 				Action: func(cCtx *cli.Context) error {
 					return createVPC(cCtx, mustCreateAPIClient(cCtx), public.ModelsAddVPC{
-						Ipv4Cidr:        cCtx.String("ipv4-cidr"),
-						Ipv6Cidr:        cCtx.String("ipv6-cidr"),
-						Description:     cCtx.String("description"),
-						OrganizationId:  cCtx.String("organization-id"),
-						PrivateCidr:     !(cCtx.String("ipv4-cidr") == "" && cCtx.String("ipv6-cidr") == ""),
-						SecurityGroupId: cCtx.String("security-group-id"),
+						Ipv4Cidr:       cCtx.String("ipv4-cidr"),
+						Ipv6Cidr:       cCtx.String("ipv6-cidr"),
+						Description:    cCtx.String("description"),
+						OrganizationId: cCtx.String("organization-id"),
+						PrivateCidr:    !(cCtx.String("ipv4-cidr") == "" && cCtx.String("ipv6-cidr") == ""),
 					})
 				},
 			},
