94dac00df2d8fc2508a78976ea27da1f6789dea8
Brent Salisbury <bsalisbu@redhat.com>
2024-01-15 10:28:38 -0500

    Update the Playwright container image
    Signed-off-by: Brent Salisbury <bsalisbu@redhat.com>

--- ui/tests/login.spec.ts
+++ ui/tests/login.spec.ts
@@ -7,7 +7,6 @@ await page.getByRole('button', { name: 'Login' }).click();
 await page.getByLabel('Username or email').fill(lib.username);
 await page.getByLabel('Password').fill(lib.password);
 await page.getByLabel('Password').press('Enter');
-await page.getByLabel('Password').press('Enter');
 
 await expect(page.getByText('Welcome to Nexodus')).toBeVisible();
 
