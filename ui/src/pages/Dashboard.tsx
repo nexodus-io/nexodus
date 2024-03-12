8c2f6de593bde1c88721e54afdc54e4a0f60a349
fedora Cloud User <fedora@ip-172-31-25-10.us-east-2.compute.internal>
2024-03-12 20:37:41 +0000

    Added device and relay nodes to Dashboard.tsx to use for Graph.tsx.

--- ui/src/pages/Dashboard.tsx
+++ ui/src/pages/Dashboard.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import {
   Card,
   CardContent,
@@ -5,14 +6,53 @@ import {
   CardMedia,
   Typography,
 } from "@mui/material";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/TableRow";
+import OnlineIcon from "@mui/icons-material/CheckCircleOutline";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 import { useTheme } from "@mui/material/styles";
 
 import CardImage from "../wordmark.png";
 import CardImageDark from "../wordmark_dark.png";
 import { backend } from "../common/Api";
 
-const Dashboard = () => {
+function createData(
+  ip: string,
+  hostname: string,
+  latency: number,
+) {
+  return { ip, hostname, latency };
+}
+
+const deviceRows = [
+  createData("100.64.0.19", "80cbb9be04cc", 25),
+  createData("100.64.0.26", "48e5326fc084", 64),
+  createData("100.64.0.25", "194712efa971", 47),
+];
+
+const relayRows = [
+  createData("100.64.0.26", "48e5326fc084", 64),
+  createData("100.64.0.19", "80cbb9be04cc", 25),
+  createData("100.64.0.25", "194712efa971", 47),
+]
+
+const Dashboard: React.FC = () => {
   const theme = useTheme();
+
+  const [isOpenDevice, setIsOpenDevice] = useState(false);
+  const [isOpenRelay, setIsOpenRelay] = useState(false);
+  const togglePopupDevice = () => {
+    setIsOpenDevice(!isOpenDevice);
+  }
+  const togglePopupRelay = () => {
+    setIsOpenRelay(!isOpenRelay);
+  }
+ 
   return (
     <div>
       <Card
@@ -48,8 +88,121 @@ const Dashboard = () => {
           </Typography>
         </CardContent>
       </Card>
-    </div>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <div style={{ marginRight: '50px'}}>
+            <button style={styles.device}
+            onClick={togglePopupDevice}>
+              Device:
+              <span style={styles.ip}>100.64.0.19</span>
+              <OnlineIcon style={styles.onlineIcon} />
+            </button>
+            {isOpenDevice && (
+                  <TableContainer component={Paper}>
+                  <Table sx={{ minWidth: 400 }} aria-label="simple table">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>IP Address</TableCell>
+                        <TableCell align="center">Hostname</TableCell>
+                        <TableCell align="center">Latency&nbsp;(ms)</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {deviceRows.map((row, index) => (
+                        <TableRow
+                          key={row.ip}
+                          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                          <TableCell component="th" scope="row" style={{textDecoration: index === 0 ? 'underline' : 'none'}}>
+                            {row.ip}
+                          </TableCell>
+                          <TableCell align="center" style={{textDecoration: index === 0 ? 'underline' : 'none'}}>{row.hostname}</TableCell>
+                          <TableCell align="center" style={{textDecoration: index === 0 ? 'underline' : 'none'}}>{row.latency}</TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              )}
+          </div>
+          <div>
+            <button style={styles.relay}
+            onClick={togglePopupRelay}>
+              Relay:
+              <span style={styles.ip}>100.64.0.26</span>
+              <HighlightOffIcon style={styles.offlineIcon} />
+            </button>
+            {isOpenRelay && (
+                  <TableContainer component={Paper}>
+                  <Table sx={{ minWidth: 400 }} aria-label="simple table">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>IP Address</TableCell>
+                        <TableCell align="center">Hostname</TableCell>
+                        <TableCell align="center">Latency&nbsp;(ms)</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {relayRows.map((row, index) => (
+                        <TableRow
+                          key={row.ip}
+                          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                          <TableCell component="th" scope="row" style={{textDecoration: index === 0 ? 'underline' : 'none'}}>
+                            {row.ip}
+                          </TableCell>
+                          <TableCell align="center" style={{textDecoration: index === 0 ? 'underline' : 'none'}}>{row.hostname}</TableCell>
+                          <TableCell align="center" style={{textDecoration: index === 0 ? 'underline' : 'none'}}>{row.latency}</TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+            )}
+          </div>
+        </div>
+      </div>
   );
+};
+
+const styles = {
+  device: {
+    fontSize: '12px',
+    fontWeight: 'bold',
+    padding: '9px 16px',
+    backgroundColor: 'green',
+    borderColor: 'green',
+    borderTopRightRadius: '20px',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  relay: {
+    fontSize: '12px',
+    fontWeight: 'bold',
+    padding: '9px 16px',
+    backgroundColor: 'red',
+    borderColor: 'red',
+    borderRadius: '50px',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  ip: {
+    fontSize: '12px',
+    fontWeight: 'bold',
+    padding: '12px 16px',
+    backgroundColor: 'gray',
+    borderColor: 'gray',
+    marginLeft: '10px',
+  },
+  onlineIcon: {
+    color: 'white',
+    fontSize: '20px',
+    marginLeft: '10px',
+  },
+  offlineIcon: {
+    color: 'white',
+    fontSize: '20px',
+    marginLeft: '10px',
+  },
 };
 
 export default Dashboard;
